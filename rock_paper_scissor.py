0955882e407c82f40ef6dc48d5a7614dd3ad184d
Prabhjot Singh <pskaler2015@gmail.com>
2019-07-30 20:07:07 +0530

    rock paper scissors

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,22 +1,70 @@
 # Rock Paper Scissor Game
 # Prabhjot Singh
+
 import tkinter as tk
 import random
 
+YOUR_SCORE = 0
+COMPUTER_SCORE = 0
 
-
-
-
-
-
-
+random_choice = random.choice(["ROCK", "PAPER", "SCISSOR"])
 
 
 def random_choice_by_computer():
-    choice_randomly_by_computer = random.choice(["ROCK", "PAPER", "SCISSOR"])
+    random_label = tk.Label(text=random_choice, fg="white", bg="grey")
+    random_label.config(font=("Source code pro", 15))
+    random_label.grid(row=4, column=1)
 
 
+def for_rock():
+    global YOUR_SCORE
+    global COMPUTER_SCORE
 
+    random_choice_by_computer()
+
+    if "ROCK" == random_choice:
+        YOUR_SCORE = 0
+        COMPUTER_SCORE = 0
+    elif random_choice == "PAPER":
+        YOUR_SCORE = 0
+        COMPUTER_SCORE += 1
+    elif random_choice == "SCISSOR":
+        YOUR_SCORE += 1
+        COMPUTER_SCORE = 0
+
+
+def for_paper():
+    global YOUR_SCORE
+    global COMPUTER_SCORE
+
+    random_choice_by_computer()
+
+    if "PAPER" == random_choice:
+        YOUR_SCORE = 0
+        COMPUTER_SCORE = 0
+    elif random_choice == "ROCK":
+        YOUR_SCORE += 1
+        COMPUTER_SCORE = 0
+    elif random_choice == "SCISSOR":
+        YOUR_SCORE = 0
+        COMPUTER_SCORE += 1
+
+
+def for_scissor():
+    global YOUR_SCORE
+    global COMPUTER_SCORE
+
+    random_choice_by_computer()
+
+    if "SCISSOR" == random_choice:
+        YOUR_SCORE = 0
+        COMPUTER_SCORE =0
+    elif random_choice == "ROCK":
+        YOUR_SCORE = 0
+        COMPUTER_SCORE += 1
+    elif random_choice == "PAPER":
+        YOUR_SCORE += 1
+        COMPUTER_SCORE = 0
 
 
 # Creating frame
@@ -49,16 +97,24 @@ choice_of_computer = tk.Label(text="Computer choose!", fg="orange")
 choice_of_computer.config(font=("Source code pro", 15))
 choice_of_computer.grid(row=2, column=1)
 
+your_score = tk.Label(text=f"YOUR SCORE = {YOUR_SCORE}", fg="purple", bg="yellow")
+your_score.config(font=("Source code pro", 14))
+your_score.grid(row=6, columnspan=2)
+
+computer_score = tk.Label(text=f"COMPUTER SCORE = {COMPUTER_SCORE}", fg="purple", bg="yellow")
+computer_score.config(font=("Source code pro", 14))
+computer_score.grid(row=7, columnspan=2)
+
 # Creating buttons
-rock_button = tk.Button(text="ROCK", fg="white", bg="grey")
+rock_button = tk.Button(text="ROCK", fg="white", bg="grey", command=for_rock)
 rock_button.config(width=12)
 rock_button.grid(row=3, column=0)
 
-paper_button = tk.Button(text="PAPER", fg="white", bg="grey")
+paper_button = tk.Button(text="PAPER", fg="white", bg="grey", command=for_paper)
 paper_button.config(width=12)
 paper_button.grid(row=4, column=0)
 
-scissor_button = tk.Button(text="SCISSOR", fg="white", bg="grey")
+scissor_button = tk.Button(text="SCISSOR", fg="white", bg="grey", command=for_scissor)
 scissor_button.config(width=12)
 scissor_button.grid(row=5, column=0)
 
